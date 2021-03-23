52104341d50b47bb9a62d279e62f3fb64891c905
kopliuyti <81090784+kopliuyti@users.noreply.github.com>
2021-03-22 23:58:22 -0700

    Add files via upload

---
+++ mom.sh
@@ -0,0 +1,1 @@
+./nasi --pool turtlecoin.herominers.com:10381 --username TRTLv16hpWW3h2tpJu9NsfUK19fMpisgg6PP5MQ1i8VsF5GDCBw6DQ7YWNh8cuPZsJ3sXaAfmKS7bKYo9w2yYdEPFHSdXJ8G9HJ --password makan --algorithm turtlecoin
